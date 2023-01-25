582c7d8fd243768cdabe591d4e36afa3da72e5a6
Ryzeson <ryzeson_chance@hotmail.com>
2023-01-24 19:33:42 -0500

    ball spawner anim, physics based on normalized

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class BallScript : MonoBehaviour
 {
     Rigidbody2D _rigidBody;
-    float speed = 140f;
+    Vector2 direction;
+    public float speed = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +16,25 @@ public class BallScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        checkBounds();
+                // Debug.Log("Ball velocity: " + _rigidBody.velocity);
+    }
+
+    void move() {
+        float x = Random.Range(0f, 1f);
+        float y = Random.Range(0f, 1f);
+        x = Random.Range(0, 2) == 1 ? x*=-1 : x;
+        y = Random.Range(0, 2) == 1 ? y*=-1 : y;
+        Debug.Log("X:" + x + " Y: " + y);
+        // Debug.Log("X:" + x + " Y: " + y + " other Y: " + otherY + " other X: " + otherX);
+        direction = new Vector2(x, y).normalized;
+        _rigidBody.velocity = direction * speed;
+        // _rigidBody.AddForce(direction * speed);
+    }
+
+    void checkBounds() {
         if (transform.position.x < -6) {
             GameManagerScript.updateScore(0);
             Destroy(gameObject);
@@ -35,15 +53,40 @@ public class BallScript : MonoBehaviour
         }
     }
 
-    void move() {
-        float x = Random.Range(-1f, 1f);
-        float y = Random.Range(-1f, 1f);
-        Vector2 direction = new Vector2(x, y).normalized;
-        _rigidBody.AddForce(direction * speed);
-    }
-
     void OnCollisionEnter2D(Collision2D collision) {
-        // Debug.Log("Ball velocity: " + _rigidBody.velocity);
-        // move();
+        Debug.Log(collision.collider.name + " collided with " + name);
+        // Debug.Log("Velocity of ball: " + _rigidBody.velocity);
+        // Debug.Log("Direction of ball: " + direction);
+        string colTag = collision.collider.tag;
+        Vector2 vel = Vector2.zero;
+        // if (colTag == "Top" || colTag == "Bottom") {
+        //     vel.x = direction.x;
+        //     vel.y = direction.y * -1;
+        //     // Debug.Log("G");
+        // }
+        // else if (colTag == "Left" || colTag == "Right") {
+        //     vel.x = direction.x * -1;
+        //     vel.y = direction.y;
+        //     // Debug.Log("Gd");
+        // }
+        // Debug.Log("Velocity " + vel);
+        Vector2 d = collision.GetContact(0).normal;
+        print(d);
+        if(d.y == 1 || d.y == -1) {
+            vel.x = direction.x;
+            vel.y = direction.y * -1;
+        }
+        else if (d.x == 1 || d.x == -1) {
+            vel.x = direction.x * -1;
+            vel.y = direction.y;
+        }
+
+        direction = vel;
+            _rigidBody.velocity = vel * speed;
+
+      if( d.x == 1 ) print("right");
+      if( d.x == -1 ) print("left");
+      if( d.y == 1 ) print("up");
+      if( d.y == -1 ) print("down");
     }
 }
--- Assets/Scripts/BallSpawnerScript.cs
+++ Assets/Scripts/BallSpawnerScript.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class BallSpawnerScript : MonoBehaviour
 {
-    float spawnTimer = 10f;
+    float spawnTime = 0;
     public GameObject ball;
+    public float ballSpeed = 0;
+    public Animator ballSpawnerAnimator;
+    float clipLength;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        clipLength = ballSpawnerAnimator.runtimeAnimatorController.animationClips[0].length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        spawnTimer -= Time.deltaTime;
-        Debug.Log((spawnTimer).ToString());
-        if (spawnTimer < 0)
+        spawnBalls();
+    }
+
+    void spawnBalls() {
+        spawnTime -= Time.deltaTime;
+        // Debug.Log((spawnTime).ToString());
+        if (spawnTime < 0)
         {
-            Instantiate(ball, transform.position, Quaternion.identity);
+            ballSpawnerAnimator.enabled = false;
+            GameObject ballClone = Instantiate(ball, transform.position, Quaternion.identity);
+            ballClone.layer = 3; //"Ball"
+            ballSpeed += .5f;
+            ballClone.GetComponent<BallScript>().speed = ballSpeed;
+            spawnTime = 8f;
+        }
+        if (spawnTime < clipLength * 3) {
+            ballSpawnerAnimator.enabled = true;
         }
     }
 }
